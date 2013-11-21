df88ad96425297f8d46b69c42dfdd82e4e316007
zeke <zeke@sikelianos.com>
2013-11-21 11:17:50 -0800

    fix up the mobile styles

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var logfmt = require('logfmt');
-
 var app = express();
 
 app.configure(function() {
   app.disable("x-powered-by");
-  app.use('/', express["static"]("public"));
   app.use(logfmt.requestLogger());
+  app.use('/', express["static"]("public"));
 });
 
 app.listen(process.env.PORT || 5000)
