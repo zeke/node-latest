10adc6c5ec5713ff0d51e0deadbc8d83cf738825
zeke <zeke@sikelianos.com>
2013-11-21 09:21:58 -0800

    add logfmt to the mix

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require("express");
+var logfmt = require('logfmt');
+
 var app = express();
 
 app.configure(function() {
   app.disable("x-powered-by");
   app.use('/', express["static"]("public"));
+  app.use(logfmt.requestLogger());
 });
 
 app.listen(process.env.PORT || 5000)
